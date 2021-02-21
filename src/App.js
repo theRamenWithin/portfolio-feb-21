124512e1d644969eed297a311f67d2f5bd68b21e
Alex Pike <55725286+theRamenWithin@users.noreply.github.com>
2021-02-21 14:04:15 +1100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="App h-screen w-full flex bg-gray-100">
+      <div className="p-12">
+        <section className="font-sans text-5xl font-bold leading-tight font-sans mb-8">
+          Hello! I’m Alex Pike, a full-stack web developer based in Sydney, Australia. I am looking for an entry-level position where I can develop my craft as a customer-focused programmer.
+        </section>
+        <section className="text-3xl leading-snug">
+          <p>
+            I just graduated from <a href="https://coderacademy.edu.au/" className="text-blue-600 hover:text-blue-800 visited:text-purple-600">Coder Academy</a> and am currently interning with <a href="https://www.healthbank.io/" className="text-blue-600 hover:text-blue-800 visited:text-purple-600">HealthBank</a> until April 1st.
+          </p>
+          <p>
+            Github
+          </p>
+        </section>
+      </div>
+    </section>
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      'body': ['Open Sans']
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
