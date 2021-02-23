abaeca78e80b58708622ed1bd6ab5aff4a88ec9d
Alex Pike <55725286+theRamenWithin@users.noreply.github.com>
2021-02-23 13:14:08 +1100

    Updated p spacing and added link to resume

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
           </p>
           {/* Links */}
           <p className="leading-snug text-lg">
-            <a href="https://github.com/theRamenWithin/" target="_blank" rel="noreferrer" className="text-blue-600 hover:underline">Github</a> / <a href="https://alexpike.work/docs/alex_pike_resume.pdf" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">Resume</a> / <a href="https://www.linkedin.com/in/alex-pike-ap/" target="_blank" rel="noreferrer" className="text-blue-600 hover:underline">LinkedIn</a> / <a href="https://twitter.com/theramenwithin/" target="_blank" rel="noreferrer" className="text-blue-600 hover:underline">Twitter</a>
+            <a href="https://github.com/theRamenWithin/" target="_blank" rel="noreferrer" className="text-blue-600 hover:underline">Github</a> / <a href="%PUBLIC_URL%/Resume_AlexPike.pdf" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">Resume</a> / <a href="https://www.linkedin.com/in/alex-pike-ap/" target="_blank" rel="noreferrer" className="text-blue-600 hover:underline">LinkedIn</a> / <a href="https://twitter.com/theramenwithin/" target="_blank" rel="noreferrer" className="text-blue-600 hover:underline">Twitter</a>
           </p>
         </section>
 
@@ -43,9 +43,9 @@ function App() {
                 <img src={ReactIcon} alt="react icon" className="w-8 mr-2"/>
                 <img src={RailsIcon} alt="rails icon" className="w-8 mr-2"/>
               </section>
-              <section className="mt-2">
-                <p>A <b>React</b> front-end, utilising Material UI, with a <b>Ruby on Rails</b> API back-end, reponding to axios requests. This project was part of my group, final assignment at Coder Academy.</p>
-                <p>AppointmentNow is a scalable, booking system customers and providers.</p>
+              <section>
+                <p className="mt-2">A <b>React</b> front-end, utilising Material UI, with a <b>Ruby on Rails</b> API back-end, reponding to axios requests. This project was part of my group, final assignment at Coder Academy.</p>
+                <p className="mt-2">AppointmentNow is a scalable, booking system customers and providers.</p>
               </section>
               <section className="mt-2">
                 <a href="https://github.com/theRamenWithin/T3A2_Project/blob/main/docs/README.md" target="_blank" rel="noreferrer" className="text-blue-600 hover:underline">View on Github</a> /
@@ -60,9 +60,9 @@ function App() {
                 <img src={RailsIcon} alt="rails icon" className="w-8 mr-2"/>
                 <img src={JSIcon} alt="javascript icon" className="w-8 mr-2"/>
               </section>
-              <section className="mt-2">
-                <p>A <b>Ruby on Rails</b> application, utilising <b>JavaScript</b> with the Twilio library. This project was the last assignment of my second term at Coder Academy.</p>
-                <p>SwordArtisanOnline is a two-way market place for Historical European Martial Arts (HEMA) equipment.</p>
+              <section>
+                <p className="mt-2">A <b>Ruby on Rails</b> application, utilising <b>JavaScript</b> with the Twilio library. This project was the last assignment of my second term at Coder Academy.</p>
+                <p className="mt-2">SwordArtisanOnline is a two-way market place for Historical European Martial Arts (HEMA) equipment.</p>
               </section>
               <section className="mt-2">
                 <a href="https://github.com/theRamenWithin/SwordArtisanOnline" target="_blank" rel="noreferrer" className="text-blue-600 hover:underline">View on Github</a> /
@@ -76,9 +76,9 @@ function App() {
               <section className="flex mt-2">
                 <img src={RailsIcon} alt="rails icon" className="w-8 mr-2"/>
               </section>
-              <section className="mt-2">
-                <p>A fully <b>Ruby on Rails</b> application, created as part of 3-day hackaton at Coder Academy in a team of 3 collaborating over <b>Git</b>.</p>
-                <p>Blogapp is a simple app for creating, reading, editing and deleting blog posts.</p>
+              <section>
+                <p className="mt-2">A fully <b>Ruby on Rails</b> application, created as part of 3-day hackaton at Coder Academy in a team of 3 collaborating over <b>Git</b>.</p>
+                <p className="mt-2">Blogapp is a simple app for creating, reading, editing and deleting blog posts.</p>
               </section>
               <section className="mt-2">
                 <a href="https://github.com/theRamenWithin/blog_app" target="_blank" rel="noreferrer" className="text-blue-600 hover:underline">View on Github</a> /
@@ -92,9 +92,9 @@ function App() {
               <section className="flex mt-2">
                 <img src={RubyIcon} alt="ruby icon" className="w-8 mr-2"/>
               </section>
-              <section className="mt-2">
-                <p>A <b>Ruby</b> terminal application, utilising third-party gems. This project was the last assignment of my first term at Coder Academy.</p>
-                <p>Dungeons Of Heck is a simple Doge themed dungeon crawler where you assume the role of a GOOD BOY to destroy HECKIN BAD THINGS across the land.</p>
+              <section>
+                <p className="mt-2">A <b>Ruby</b> terminal application, utilising third-party gems. This project was the last assignment of my first term at Coder Academy.</p>
+                <p className="mt-2">Dungeons Of Heck is a simple Doge themed dungeon crawler where you assume the role of a GOOD BOY to destroy HECKIN BAD THINGS across the land.</p>
               </section>
               <section className="mt-2">
                 <a href="https://github.com/theRamenWithin/DungeonsOfHeck" target="_blank" rel="noreferrer" className="text-blue-600 hover:underline">View on Github</a> /
@@ -109,8 +109,8 @@ function App() {
                 <img src={HTML5Icon} alt="html5 icon" className="w-8 mr-2"/>
                 <img src={CSS3Icon} alt="css3 icon" className="w-8 mr-2"/>
               </section>
-              <section className="mt-2">
-                <p>A portfolio website written in vanilla <b>HTML5</b> & <b>CSS3</b>. This project was the second assignment of my first term at Coder Academy and the first website I ever made.</p>
+              <section>
+                <p className="mt-2">A portfolio website written in vanilla <b>HTML5</b> & <b>CSS3</b>. This project was the second assignment of my first term at Coder Academy and the first website I ever made.</p>
               </section>
               <section className="mt-2">
                 <a href="https://github.com/theRamenWithin/portfolio-sep-20" target="_blank" rel="noreferrer" className="text-blue-600 hover:underline">View on Github</a> /
