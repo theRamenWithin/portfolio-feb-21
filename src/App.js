d1233352ab06f9a2cac2f6953a37b67848c4cf49
Alex Pike <55725286+theRamenWithin@users.noreply.github.com>
2021-02-23 11:53:43 +1100

    Added descriptive text to project tiles

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import ReactIcon from './assets/icons/react.svg'
 function App() {
   return (
     // Window container
-    <section className="App h-full w-full flex bg-white">
+    <section className="App h-full w-full flex">
       {/* Content container */}
       <div className="p-12">
 
@@ -43,7 +43,8 @@ function App() {
                 <img src={ReactIcon} alt="rails icon" className="w-12"/>
                 <img src={RailsIcon} alt="rails icon" className="w-12"/>
               </section>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laudantium dolor obcaecati illum voluptates voluptatibus corrupti! Ut qui cupiditate tempora.</p>
+              <p>A <u>React</u> front-end, utilising Material UI, with a <u>Ruby on Rails</u> API back-end, reponding to axios requests. This project was part of my group, final assignment at Coder Academy.</p>
+              <p>AppointmentNow is a scalable, booking system customers and providers.</p>
               <section className="mt-2">
                 <a href="https://github.com/theRamenWithin/T3A2_Project/blob/main/docs/README.md" className="text-blue-600 hover:text-blue-800 visited:text-purple-600">View on Github</a>
                 <a href="https://appointmentnow.netlify.app/" className="text-blue-600 hover:text-blue-800 visited:text-purple-600 ml-10">View Live</a>
@@ -57,7 +58,8 @@ function App() {
                 <img src={RailsIcon} alt="rails icon" className="w-12"/>
                 <img src={JSIcon} alt="rails icon" className="w-12"/>
               </section>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laudantium dolor obcaecati illum voluptates voluptatibus corrupti! Ut qui cupiditate tempora.</p>
+              <p>A <u>Ruby on Rails</u> application, utilising <u>JavaScript</u> with the Twilio library. This project was the last assignment of my second term at Coder Academy.</p>
+              <p>SwordArtisanOnline is a two-way market place for Historical European Martial Arts (HEMA) equipment.</p>
               <section className="mt-2">
                 <a href="https://github.com/theRamenWithin/SwordArtisanOnline" className="text-blue-600 hover:text-blue-800 visited:text-purple-600">View on Github</a>
                 <a href="https://swordartisanonline.herokuapp.com/" className="text-blue-600 hover:text-blue-800 visited:text-purple-600 ml-10">View Live</a>
@@ -70,7 +72,8 @@ function App() {
               <section className="flex mt-2">
                 <img src={RailsIcon} alt="rails icon" className="w-12"/>
               </section>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laudantium dolor obcaecati illum voluptates voluptatibus corrupti! Ut qui cupiditate tempora.</p>
+              <p>A fully <u>Ruby on Rails</u> application, created as part of 3-day hackaton at Coder Academy in a team of 3 collaborating over <u>Git</u>.</p>
+              <p>Blogapp is a simple app for creating, reading, editing and deleting blog posts.</p>
               <section className="mt-2">
                 <a href="https://github.com/theRamenWithin/blog_app" className="text-blue-600 hover:text-blue-800 visited:text-purple-600">View on Github</a>
                 <a href="https://blogapp-2020.herokuapp.com/" className="text-blue-600 hover:text-blue-800 visited:text-purple-600 ml-10">View Live</a>
@@ -83,7 +86,8 @@ function App() {
               <section className="flex mt-2">
                 <img src={RubyIcon} alt="rails icon" className="w-12"/>
               </section>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laudantium dolor obcaecati illum voluptates voluptatibus corrupti! Ut qui cupiditate tempora.</p>
+              <p>A <u>Ruby</u> terminal application, utilising third-party gems. This project was the last assignment of my first term at Coder Academy.</p>
+              <p>Dungeons Of Heck is a simple Doge themed dungeon crawler where you assume the role of a GOOD BOY to destroy HECKIN BAD THINGS across the land.</p>
               <section className="mt-2">
                 <a href="https://github.com/theRamenWithin/DungeonsOfHeck" className="text-blue-600 hover:text-blue-800 visited:text-purple-600">View on Github</a>
                 <a href="https://rubygems.org/gems/dungeonsofheck" className="text-blue-600 hover:text-blue-800 visited:text-purple-600 ml-10">View on RubyGems</a>
@@ -97,7 +101,7 @@ function App() {
                 <img src={HTML5Icon} alt="rails icon" className="w-12"/>
                 <img src={CSS3Icon} alt="rails icon" className="w-12"/>
               </section>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laudantium dolor obcaecati illum voluptates voluptatibus corrupti! Ut qui cupiditate tempora.</p>
+              <p>A portfolio website written in vanilla <u>HTML5</u> & <u>CSS3</u>. This project was the second assignment of my first term at Coder Academy and the first website I ever made.</p>
               <section className="mt-2">
                 <a href="https://github.com/theRamenWithin/portfolio-sep-20" className="text-blue-600 hover:text-blue-800 visited:text-purple-600">View on Github</a>
                 <a href="https://alexpikeap.netlify.app" className="text-blue-600 hover:text-blue-800 visited:text-purple-600 ml-10">View Live</a>
