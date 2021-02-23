d164a3a5e0f5e075e867a9656edf99bcb6645955
Alex Pike <55725286+theRamenWithin@users.noreply.github.com>
2021-02-23 12:56:34 +1100

    Fixed links, font, text colors and spacing

--- src/App.js
+++ src/App.js
@@ -8,103 +8,113 @@ import ReactIcon from './assets/icons/react.svg'
 function App() {
   return (
     // Window container
-    <section className="App h-full w-full flex">
+    <section className="App h-full w-full flex font-sans text-gray-600">
       {/* Content container */}
       <div className="p-12">
 
         {/* Lead text */}
-        <section className="font-sans text-5xl font-bold leading-tight font-sans mb-8">
+        <section className="text-5xl font-bold text-gray-800 leading-tight mb-4">
           Hello! I’m Alex Pike, a full-stack web developer based in Sydney, Australia. I am looking for an entry-level position where I can develop my craft as a customer-focused programmer.
         </section>
         
         <section>
           {/* Sub text */}
           <p className="mb-4 text-3xl leading-snug">
-            I just graduated from <a href="https://coderacademy.edu.au/" className="text-blue-600 hover:text-blue-800 visited:text-purple-600">Coder Academy</a> and am currently interning with <a href="https://www.healthbank.io/" className="text-blue-600 hover:text-blue-800 visited:text-purple-600">HealthBank</a> until April 1st.
+            I just graduated from <a href="https://coderacademy.edu.au/" target="_blank" rel="noreferrer" className="text-blue-600 hover:underline">Coder Academy</a> and am currently interning with <a href="https://www.healthbank.io/" target="_blank" rel="noreferrer" className="text-blue-600 hover:underline">HealthBank</a> until April 1st.
           </p>
           {/* Links */}
           <p className="leading-snug text-lg">
-            <a href="https://github.com/theRamenWithin/" className="text-blue-600 hover:text-blue-800 visited:text-purple-600">Github</a> / <a href="https://alexpike.work/docs/alex_pike_resume.pdf" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:text-blue-800 visited:text-purple-600">Resume</a> / <a href="https://www.linkedin.com/in/alex-pike-ap/" className="text-blue-600 hover:text-blue-800 visited:text-purple-600">LinkedIn</a> / <a href="https://twitter.com/theramenwithin/" className="text-blue-600 hover:text-blue-800 visited:text-purple-600">Twitter</a>
+            <a href="https://github.com/theRamenWithin/" target="_blank" rel="noreferrer" className="text-blue-600 hover:underline">Github</a> / <a href="https://alexpike.work/docs/alex_pike_resume.pdf" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">Resume</a> / <a href="https://www.linkedin.com/in/alex-pike-ap/" target="_blank" rel="noreferrer" className="text-blue-600 hover:underline">LinkedIn</a> / <a href="https://twitter.com/theramenwithin/" target="_blank" rel="noreferrer" className="text-blue-600 hover:underline">Twitter</a>
           </p>
         </section>
 
         {/* Project container */}
-        <section className="mt-6 text-lg">
-          <div className="font-mono">
-            <p>My Work</p>
+        <section className="mt-16">
+          <div className="font-mono text-2xl">
+            <p>My Projects</p>
           </div>
           {/* Project tiles */}
-          <section className="flex flex-wrap justify-between mt-6">
+          <section className="flex flex-wrap justify-between mt-6 text-lg">
 
             {/* Project tile */}
             <div className="max-w-sm mb-6">
-              <p className="font-bold mb-2">AppointmentNow</p>
+              <p className="font-bold mb-2 text-gray-800">AppointmentNow</p>
               <section className="flex mt-2">
-                <img src={ReactIcon} alt="rails icon" className="w-12"/>
-                <img src={RailsIcon} alt="rails icon" className="w-12"/>
+                <img src={ReactIcon} alt="react icon" className="w-8 mr-2"/>
+                <img src={RailsIcon} alt="rails icon" className="w-8 mr-2"/>
               </section>
-              <p>A <u>React</u> front-end, utilising Material UI, with a <u>Ruby on Rails</u> API back-end, reponding to axios requests. This project was part of my group, final assignment at Coder Academy.</p>
-              <p>AppointmentNow is a scalable, booking system customers and providers.</p>
               <section className="mt-2">
-                <a href="https://github.com/theRamenWithin/T3A2_Project/blob/main/docs/README.md" className="text-blue-600 hover:text-blue-800 visited:text-purple-600">View on Github</a>
-                <a href="https://appointmentnow.netlify.app/" className="text-blue-600 hover:text-blue-800 visited:text-purple-600 ml-10">View Live</a>
+                <p>A <b>React</b> front-end, utilising Material UI, with a <b>Ruby on Rails</b> API back-end, reponding to axios requests. This project was part of my group, final assignment at Coder Academy.</p>
+                <p>AppointmentNow is a scalable, booking system customers and providers.</p>
+              </section>
+              <section className="mt-2">
+                <a href="https://github.com/theRamenWithin/T3A2_Project/blob/main/docs/README.md" target="_blank" rel="noreferrer" className="text-blue-600 hover:underline">View on Github</a> /
+                <a href="https://appointmentnow.netlify.app/" target="_blank" rel="noreferrer" className="text-blue-600 hover:underline ml-2">View Live</a>
               </section>
             </div>
 
             {/* Project tile */}
             <div className="max-w-sm mb-6">
-              <p className="font-bold mb-2">SwordArtisanOnline</p>
+              <p className="font-bold mb-2 text-gray-800">SwordArtisanOnline</p>
               <section className="flex mt-2">
-                <img src={RailsIcon} alt="rails icon" className="w-12"/>
-                <img src={JSIcon} alt="rails icon" className="w-12"/>
+                <img src={RailsIcon} alt="rails icon" className="w-8 mr-2"/>
+                <img src={JSIcon} alt="javascript icon" className="w-8 mr-2"/>
               </section>
-              <p>A <u>Ruby on Rails</u> application, utilising <u>JavaScript</u> with the Twilio library. This project was the last assignment of my second term at Coder Academy.</p>
-              <p>SwordArtisanOnline is a two-way market place for Historical European Martial Arts (HEMA) equipment.</p>
               <section className="mt-2">
-                <a href="https://github.com/theRamenWithin/SwordArtisanOnline" className="text-blue-600 hover:text-blue-800 visited:text-purple-600">View on Github</a>
-                <a href="https://swordartisanonline.herokuapp.com/" className="text-blue-600 hover:text-blue-800 visited:text-purple-600 ml-10">View Live</a>
+                <p>A <b>Ruby on Rails</b> application, utilising <b>JavaScript</b> with the Twilio library. This project was the last assignment of my second term at Coder Academy.</p>
+                <p>SwordArtisanOnline is a two-way market place for Historical European Martial Arts (HEMA) equipment.</p>
+              </section>
+              <section className="mt-2">
+                <a href="https://github.com/theRamenWithin/SwordArtisanOnline" target="_blank" rel="noreferrer" className="text-blue-600 hover:underline">View on Github</a> /
+                <a href="https://swordartisanonline.herokuapp.com/" target="_blank" rel="noreferrer" className="text-blue-600 hover:underline ml-2">View Live</a>
               </section>
             </div>
 
             {/* Project tile */}
             <div className="max-w-sm mb-6">
-              <p className="font-bold mb-2">Blogapp</p>
+              <p className="font-bold mb-2 text-gray-800">Blogapp</p>
               <section className="flex mt-2">
-                <img src={RailsIcon} alt="rails icon" className="w-12"/>
+                <img src={RailsIcon} alt="rails icon" className="w-8 mr-2"/>
               </section>
-              <p>A fully <u>Ruby on Rails</u> application, created as part of 3-day hackaton at Coder Academy in a team of 3 collaborating over <u>Git</u>.</p>
-              <p>Blogapp is a simple app for creating, reading, editing and deleting blog posts.</p>
               <section className="mt-2">
-                <a href="https://github.com/theRamenWithin/blog_app" className="text-blue-600 hover:text-blue-800 visited:text-purple-600">View on Github</a>
-                <a href="https://blogapp-2020.herokuapp.com/" className="text-blue-600 hover:text-blue-800 visited:text-purple-600 ml-10">View Live</a>
+                <p>A fully <b>Ruby on Rails</b> application, created as part of 3-day hackaton at Coder Academy in a team of 3 collaborating over <b>Git</b>.</p>
+                <p>Blogapp is a simple app for creating, reading, editing and deleting blog posts.</p>
+              </section>
+              <section className="mt-2">
+                <a href="https://github.com/theRamenWithin/blog_app" target="_blank" rel="noreferrer" className="text-blue-600 hover:underline">View on Github</a> /
+                <a href="https://blogapp-2020.herokuapp.com/" target="_blank" rel="noreferrer" className="text-blue-600 hover:underline ml-2">View Live</a>
               </section>
             </div>
 
             {/* Project tile */}
             <div className="max-w-sm mb-6">
-              <p className="font-bold mb-2">Dungeons of Heck</p>
+              <p className="font-bold mb-2 text-gray-800">Dungeons of Heck</p>
               <section className="flex mt-2">
-                <img src={RubyIcon} alt="rails icon" className="w-12"/>
+                <img src={RubyIcon} alt="ruby icon" className="w-8 mr-2"/>
               </section>
-              <p>A <u>Ruby</u> terminal application, utilising third-party gems. This project was the last assignment of my first term at Coder Academy.</p>
-              <p>Dungeons Of Heck is a simple Doge themed dungeon crawler where you assume the role of a GOOD BOY to destroy HECKIN BAD THINGS across the land.</p>
               <section className="mt-2">
-                <a href="https://github.com/theRamenWithin/DungeonsOfHeck" className="text-blue-600 hover:text-blue-800 visited:text-purple-600">View on Github</a>
-                <a href="https://rubygems.org/gems/dungeonsofheck" className="text-blue-600 hover:text-blue-800 visited:text-purple-600 ml-10">View on RubyGems</a>
+                <p>A <b>Ruby</b> terminal application, utilising third-party gems. This project was the last assignment of my first term at Coder Academy.</p>
+                <p>Dungeons Of Heck is a simple Doge themed dungeon crawler where you assume the role of a GOOD BOY to destroy HECKIN BAD THINGS across the land.</p>
+              </section>
+              <section className="mt-2">
+                <a href="https://github.com/theRamenWithin/DungeonsOfHeck" target="_blank" rel="noreferrer" className="text-blue-600 hover:underline">View on Github</a> /
+                <a href="https://rubygems.org/gems/dungeonsofheck" target="_blank" rel="noreferrer" className="text-blue-600 hover:underline ml-2">View on RubyGems</a>
               </section>
             </div>
             
             {/* Project tile */}
             <div className="max-w-sm mb-6">
-              <p className="font-bold mb-2">Portfolio Website</p>
+              <p className="font-bold mb-2 text-gray-800">Example Portfolio Website</p>
               <section className="flex mt-2">
-                <img src={HTML5Icon} alt="rails icon" className="w-12"/>
-                <img src={CSS3Icon} alt="rails icon" className="w-12"/>
+                <img src={HTML5Icon} alt="html5 icon" className="w-8 mr-2"/>
+                <img src={CSS3Icon} alt="css3 icon" className="w-8 mr-2"/>
               </section>
-              <p>A portfolio website written in vanilla <u>HTML5</u> & <u>CSS3</u>. This project was the second assignment of my first term at Coder Academy and the first website I ever made.</p>
               <section className="mt-2">
-                <a href="https://github.com/theRamenWithin/portfolio-sep-20" className="text-blue-600 hover:text-blue-800 visited:text-purple-600">View on Github</a>
-                <a href="https://alexpikeap.netlify.app" className="text-blue-600 hover:text-blue-800 visited:text-purple-600 ml-10">View Live</a>
+                <p>A portfolio website written in vanilla <b>HTML5</b> & <b>CSS3</b>. This project was the second assignment of my first term at Coder Academy and the first website I ever made.</p>
+              </section>
+              <section className="mt-2">
+                <a href="https://github.com/theRamenWithin/portfolio-sep-20" target="_blank" rel="noreferrer" className="text-blue-600 hover:underline">View on Github</a> /
+                <a href="https://alexpikeap.netlify.app" target="_blank" rel="noreferrer" className="text-blue-600 hover:underline ml-2">View Live</a>
               </section>
             </div>
 
